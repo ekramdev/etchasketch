37902691de0da97622680da56a106e4eaf15217e
@ekramdev <karoom282030@gmail.com>
2025-08-28 15:41:20 +0300

    add divs to container

---
+++ script.js
@@ -0,0 +1,63 @@
+// creating 16 X 16 of square divs
+const container = document.getElementById("container");
+container.style.cssText= "width:700px ; height:700px ; border:2px solid white; background-color:black;margin :100px auto; display: flex";
+const div1 = document.getElementsByClassName("div1")[0];
+div1.style.cssText = "width:100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div2 = document.getElementsByClassName("div2") [0];
+div2.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div3 = document.getElementsByClassName("div3") [0];
+div3.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div4 = document.getElementsByClassName("div4") [0];
+div4.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div5 = document.getElementsByClassName("div5") [0];
+div5.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div6 = document.getElementsByClassName("div6") [0];
+div6.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div7 = document.getElementsByClassName("div7") [0];
+div7.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div8 = document.getElementsByClassName("div8") [0];
+div8.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div9 = document.getElementsByClassName("div9") [0];
+div9.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div10 = document.getElementsByClassName("div10") [0];
+div10.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div11 = document.getElementsByClassName("div11") [0];
+div11.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div12 = document.getElementsByClassName("div12") [0];
+div12.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div13 = document.getElementsByClassName("div13") [0];
+div13.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div14 = document.getElementsByClassName("div14")[0];
+div14.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div15 = document.getElementsByClassName("div15") [0];
+div15.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+const div16 = document.getElementsByClassName("div16") [0];
+div16.style.cssText = "width: 100px ; height :100px ; border: 2px solid white; background-color:; black";
+
+// const allDivs = document.querySelectorAll("div");
+
+// allDivs.forEach( div => {
+//     div.style.cssText = "display: flex;";
+// });
+
+
+
+
+container.appendChild(div1);
+container.appendChild(div2);
+container.appendChild(div3);
+container.appendChild(div4);
+container.appendChild(div5);
+container.appendChild(div6);
+container.appendChild(div7);
+container.appendChild(div8);
+container.appendChild(div9);
+container.appendChild(div10);
+container.appendChild(div11);
+container.appendChild(div12);
+container.appendChild(div13);
+container.appendChild(div14);
+container.appendChild(div15);
+container.appendChild(div16);
+document.body.appendChild(container);
+
